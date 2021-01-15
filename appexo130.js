d2010ca45ea7209546505730309de521f2898ebd
Sylvie-B <unsolo@hotmail.fr>
2021-01-15 10:49:37 +0100

    textarea

--- appexo130.js
+++ appexo130.js
@@ -1,5 +1,6 @@
 // 1. déactiver envoi si pas checkbox
 
+
 // 2. corriger prbl src d'img
 
 // 3. blink text
@@ -13,4 +14,21 @@ setInterval(blinks, 1000);
 // 4.
 $('tr').siblings(':even').css('background', 'grey');
 
-// 5.
+// 5. imprimer
+
+
+// 6.
+let nbr = 250;
+$('#car').text(nbr);
+
+$('#message').keyup(function () {
+    let text = $('#message').val().length;
+    let left = nbr - text;
+    if(left > 0){
+        $('#car').text(left);
+    }
+    else {
+        alert('il reste ' + left + ' caractere');
+    }
+})
+
